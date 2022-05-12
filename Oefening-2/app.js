f27ff65d2371ee247f1eb4d3d011643dda5ff6d1
Ullavs <ullavansprang@gmail.com>
2022-05-12 23:01:36 +0200

    Part 1 - calculator JS

--- Oefening-2/app.js
+++ Oefening-2/app.js
@@ -1,12 +1,57 @@
 const myInput = document.getElementById("result");
 const myOperator = document.getElementById("operator");
+let firstNumber;
 
 function onClickNumber(clickedNumber) {
-  myInput.value = clickedNumber;
+  if (clickedNumber === "." && myInput.value.includes(".")) {
+    return;
+  }
+
+  if (clickedNumber === "0" && myInput.value.includes("0")) {
+    return;
+  }
+
+  if (myOperator.value === "=") {
+    myInput.value = "";
+    myOperator.value = "";
+  }
+
+  myInput.value += clickedNumber;
+
+  // maximaal 1 "0" aan de voorkant
+  // 0.1 + 0.2 oplossen
 }
 
 function onClickOperator(clickedOperator) {
   myOperator.value = clickedOperator;
+
+  firstNumber = Number(myInput.value);
+  myInput.value = "";
+  console.log(firstNumber);
+}
+
+function onClickEquals(clickedEquals) {
+  let secondNumber = Number(myInput.value);
+
+  switch (myOperator.value) {
+    case "+":
+      myInput.value = firstNumber + secondNumber;
+      myOperator.value = "=";
+      break;
+    case "-":
+      myInput.value = firstNumber - secondNumber;
+      myOperator.value = "=";
+      break;
+    case "x":
+      myInput.value = firstNumber * secondNumber;
+      myOperator.value = "=";
+      break;
+    case "/":
+      myInput.value = firstNumber / secondNumber;
+      myOperator.value = "=";
+  }
+
+  console.log(secondNumber);
 }
 
 function onClickCancel(clickedCancel) {
@@ -14,6 +59,18 @@ function onClickCancel(clickedCancel) {
   myOperator.value = "";
 }
 
-function onClickEquals(clickedEquals) {
-  console.log(clickedEquals);
-}
+// na operator geen "0" maar echt LEEG
+// 1 + 2 + 3 doet het nog niet!!!!
+
+// Hoe evalueert u of er al een waarde in myInput staat?
+// Er staat weliswaar een 0 in het display maar dit is een placeholder, géén waarde.
+// De waarde in de initiële staat is "". Als dit true is, mag een nieuw getal ingevoerd worden.
+
+// Klikken op operator --> ingevoerde cijfer opslaan --> vervolgens nieuw cijfer kunnen invoeren
+
+// Hoe zorgt u ervoor dat er niet een aantal nullen achter elkaar ingetoetst kunnen worden
+// tenzij deze vooraf gegaan zijn door een ander cijfer of een punt?
+
+// U hebt een aantal if / else if / else statements nodig
+// én u moet de invoerwaarden van de berekeningen in variabelen (let) opslaan.
+// Ook moet u de gekozen operator in een let opslaan.
